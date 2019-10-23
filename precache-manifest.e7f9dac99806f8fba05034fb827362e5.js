461541c17146f019cf3d6802de7a89a2872aa7cb
Olagunju Adewale <adewale.olagunju@siliconharbourng.com>
2019-10-23 10:31:47 +0100

    Updates

---
+++ precache-manifest.e7f9dac99806f8fba05034fb827362e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c94b46785ba659732157b567b88d5690",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "6eb0c3eccc7182284e89",
+    "url": "/website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c8f8a32fac16378745c0",
+    "url": "/website/static/js/2.f87c5d5a.chunk.js"
+  },
+  {
+    "revision": "6eb0c3eccc7182284e89",
+    "url": "/website/static/js/main.cc732a31.chunk.js"
+  },
+  {
+    "revision": "883d895f9696ce839c6c",
+    "url": "/website/static/js/runtime-main.47783ef3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website/static/media/logo.25bf045c.svg"
+  }
+]);
